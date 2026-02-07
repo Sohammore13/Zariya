a1f3f09a2c39b349e814d71c286554ec883d6905
Sohammore13 <sohammore136@gmail.com>
2026-02-07 21:53:22 +0530

    authorization

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,8 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import path from "path";
 
+import { connectDB } from "./lib/db.js";
+
 dotenv.config();
 
 const app = express();
@@ -11,6 +13,11 @@ const app = express();
 const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json()); // req.body
+
+
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes)
@@ -28,6 +35,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+app.listen(PORT, () => {
+    console.log("Server is running on port: " + PORT);
+    connectDB();
 });
